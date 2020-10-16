8d9f1e3e7c94c961bf8fce0223cfdebb71dcf3a3
Mikael Samvelian <msamvelian@haivision.com>
2020-10-15 21:48:07 -0400

    Fix href warning

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -21,13 +21,13 @@ const Navbar = () => {
                         </div>
                         <div className="hidden sm:ml-6 sm:flex">
                             <a
-                                href="#"
+                                href="/"
                                 className="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
                             >
                                 Calculator
                             </a>
                             <a
-                                href="#"
+                                href="/"
                                 className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
                             >
                                 Blog
@@ -76,25 +76,25 @@ const Navbar = () => {
             <div className="hidden sm:hidden">
                 <div className="pt-2 pb-3">
                     <a
-                        href="#"
+                        href="/"
                         className="block pl-3 pr-4 py-2 border-l-4 border-indigo-500 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out"
                     >
                         Dashboard
                     </a>
                     <a
-                        href="#"
+                        href="/"
                         className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
                     >
                         Team
                     </a>
                     <a
-                        href="#"
+                        href="/"
                         className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
                     >
                         Projects
                     </a>
                     <a
-                        href="#"
+                        href="/"
                         className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
                     >
                         Calendar
@@ -120,19 +120,19 @@ const Navbar = () => {
                     </div>
                     <div className="mt-3">
                         <a
-                            href="#"
+                            href="/"
                             className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
                         >
                             Your Profile
                         </a>
                         <a
-                            href="#"
+                            href="/"
                             className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
                         >
                             Settings
                         </a>
                         <a
-                            href="#"
+                            href="/"
                             className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
                         >
                             Sign out
