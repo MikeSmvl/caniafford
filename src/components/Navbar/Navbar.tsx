4b95f1c195cad1fd665e795ac3c16092f3d39d43
Mikael Samvelian <mikael.samvelian@gmail.com>
2020-10-19 21:44:31 -0400

    Add can i afford logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                                 href="/"
                                 className="inline-flex items-center px-1 pt-1 border-b-2 border-teal-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-teal-700 transition duration-150 ease-in-out"
                             >
-                                Calculator
+                                Home
                             </a>
                             <button className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 opacity-50 cursor-not-allowed">
                                 Blog
